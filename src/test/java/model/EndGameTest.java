b5ece50715d2393ff985173af780a9bd82ffd4ec
heinjl <heinjl@rose-hulman.edu>
2017-03-15 15:01:19 -0400

    Completed a stalemate test using a game from Korchnoi vs. Karpov

--- src/test/java/model/EndGameTest.java
+++ src/test/java/model/EndGameTest.java
@@ -16,25 +16,32 @@ import utility.LoadMoves;
 
 public class EndGameTest {
 
+	private Chess setupChessGame(String fileName) throws FileNotFoundException {
+		Chess chess = new Chess();
+		assertTrue(LoadMoves.performRecordMoves(chess, fileName));
+		assertTrue(chess.getRecords().hasEnd());
+		
+		return chess;
+	}
+	
 	@Test
 	public void testWhiteCheckmate() throws FileNotFoundException {
-		Chess chess = new Chess();
-		
-		assertTrue(LoadMoves.performRecordMoves(chess, "sampleGames/White_Checkmate.txt"));
-		
-		assertTrue(chess.getRecords().hasEnd());
+		Chess chess = setupChessGame("sampleGames/White_Checkmate.txt");
 		
 		assertEquals(1, chess.getRecords().getEndGame().getResult());
 	}
 	
 	@Test
 	public void testBlackCheckmate() throws FileNotFoundException {
-		Chess chess = new Chess();
-		
-		assertTrue(LoadMoves.performRecordMoves(chess, "sampleGames/Black_Checkmate.txt"));
-		
-		assertTrue(chess.getRecords().hasEnd());
+		Chess chess = setupChessGame("sampleGames/Black_Checkmate.txt");
 		
 		assertEquals(-1, chess.getRecords().getEndGame().getResult());
+	}
+	
+	@Test
+	public void testStalemate() throws FileNotFoundException {
+		Chess chess = setupChessGame("sampleGames/Stalemate.txt");
+		
+		assertEquals(0, chess.getRecords().getEndGame().getResult());
 	}
 }

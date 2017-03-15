83c0b700c8ec32646ba8c368cf0bd144b7023847
heinjl <heinjl@rose-hulman.edu>
2017-03-15 15:11:14 -0400

    Fixed uneeded imports in the EndGameTest

--- src/test/java/model/EndGameTest.java
+++ src/test/java/model/EndGameTest.java
@@ -3,15 +3,11 @@ package model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 import org.junit.Test;
 
 import model.Chess;
-import model.InvalidMoveException;
-import model.Move;
 import utility.LoadMoves;
 
 public class EndGameTest {

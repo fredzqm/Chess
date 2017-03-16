1047ae09ac959ead1f9a6415484c54bd031d7f71
Fred Zhang <fredzqm@gmail.com>
2017-03-16 17:20:50 -0400

    refactor EnPassant test to use test utitlities

--- src/test/java/model/ChessEnPassentTest.java
+++ src/test/java/model/ChessEnPassentTest.java
@@ -1,21 +1,25 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.List;
+
 import org.junit.Test;
+
+import utility.TestUtitlities;
 
 public class ChessEnPassentTest {
 	
 	@Test
 	public void testWhiteEnPassent() throws FileNotFoundException, InvalidMoveException {
 		String file = "sampleGames/White_EnPassent.txt";
-		String lastMove = getLastMoveInFile(file);
-		
+		List<String> moveLs = TestUtitlities.getMoveString(file);
+		String lastMove = moveLs.get(moveLs.size()-1);
+
 		Chess chess = new Chess();
-		performRecordMovesButLast(chess, file);
+		TestUtitlities.performRecordMoves(chess, moveLs.subList(0, moveLs.size()-1));
 		
 		String move = lastMove.split("-")[1];
 		int x = findIndex(move.charAt(0));
@@ -27,10 +31,11 @@ public class ChessEnPassentTest {
 	@Test
 	public void TestWhiteInvalidWhiteEnPassent() throws FileNotFoundException, InvalidMoveException{
 		String file = "sampleGames/White_InvalidEnPassent.txt";
-		String lastMove = getLastMoveInFile(file);
+		List<String> moveLs = TestUtitlities.getMoveString(file);
+		String lastMove = moveLs.get(moveLs.size()-1);
 		
 		Chess chess = new Chess();
-		performRecordMovesButLast(chess, file);
+		TestUtitlities.performRecordMoves(chess, moveLs.subList(0, moveLs.size()-1));
 		
 		String move = lastMove.split("-")[1];
 		int x = findIndex(move.charAt(0));
@@ -42,10 +47,11 @@ public class ChessEnPassentTest {
 	@Test
 	public void testBlackEnPassent() throws FileNotFoundException, InvalidMoveException {
 		String file = "sampleGames/Black_EnPassent.txt";
-		String lastMove = getLastMoveInFile(file);
+		List<String> moveLs = TestUtitlities.getMoveString(file);
+		String lastMove = moveLs.get(moveLs.size()-1);
 
 		Chess chess = new Chess();
-		performRecordMovesButLast(chess, file);
+		TestUtitlities.performRecordMoves(chess, moveLs.subList(0, moveLs.size()-1));
 		
 		String move = lastMove.split("-")[1];
 		int x = findIndex(move.charAt(0));
@@ -57,10 +63,11 @@ public class ChessEnPassentTest {
 	@Test
 	public void TestWhiteInvalidEnPassent() throws FileNotFoundException, InvalidMoveException{
 		String file = "sampleGames/Black_InvalidEnPassent.txt";
-		String lastMove = getLastMoveInFile(file);
+		List<String> moveLs = TestUtitlities.getMoveString(file);
+		String lastMove = moveLs.get(moveLs.size()-1);
 		
 		Chess chess = new Chess();
-		performRecordMovesButLast(chess, file);
+		TestUtitlities.performRecordMoves(chess, moveLs.subList(0, moveLs.size()-1));
 		
 		String move = lastMove.split("-")[1];
 		int x = findIndex(move.charAt(0));
@@ -78,57 +85,4 @@ public class ChessEnPassentTest {
 		return index;
 	}
 
-	private String getLastMoveInFile(String file) throws FileNotFoundException {
-		Scanner scan = new Scanner(new File(file));
-		String lastMove = "";
-		while (scan.hasNextLine()) {
-			String line = scan.nextLine();
-
-			if (line.startsWith("["))
-				continue;
-
-			String[] parts = line.split("\\d+\\.| ");
-			for (int i = 0; i < parts.length; i++) {
-				if (parts[i].length() == 0)
-					continue;
-				lastMove = parts[i];
-			}
-		}
-
-		scan.close();
-		return lastMove;
-	}
-
-	/**
-	 * Read a file and perform the moves recorded in the file.
-	 * 
-	 * @param filename
-	 * @throws FileNotFoundException
-	 */
-	private void performRecordMovesButLast(Chess chess, String filename) throws FileNotFoundException {
-		Scanner scan = new Scanner(new File(filename));
-
-		while (scan.hasNextLine()) {
-			String line = scan.nextLine();
-
-			if (line.startsWith("["))
-				continue;
-
-			String[] parts = line.split("\\d+\\.| ");
-			for (int i = 0; i < parts.length - 1; i++) {
-				if (parts[i].length() == 0)
-					continue;
-
-				Move move;
-				try {
-					move = chess.getMove(parts[i]);
-					chess.makeMove(move);
-				} catch (InvalidMoveException e) {
-					System.out.println(parts[i] + "\n");
-				}
-			}
-		}
-
-		scan.close();
-	}
 }

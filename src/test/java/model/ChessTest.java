a3f9d72839433de6f729b12c663d519310fb8b53
Fred Zhang <fredzqm@gmail.com>
2017-03-17 14:35:01 -0400

    add test for getSquare()

--- src/test/java/model/ChessTest.java
+++ src/test/java/model/ChessTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ChessTest {
 
 	@Test
-	public void test() {
+	public void testSpotAt() {
 		Chess chess = new Chess();
 
 		for (int i = 1; i <= 8; i++) {
@@ -15,9 +15,26 @@ public class ChessTest {
 				Square square = chess.spotAt(i, j);
 				char a = (char) (i - 1 + 'a');
 				char b = (char) (j - 1 + '1');
+				
 				assertEquals("" + a + b, square.toString());
 			}
 		}
 	}
 
+
+	@Test
+	public void testgetSquare() {
+		Chess chess = new Chess();
+
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 8; j++) {
+				Square square = chess.spotAt(i, j);
+				char a = (char) (i - 1 + 'a');
+				char b = (char) (j - 1 + '1');
+				Square squareGot = chess.getSquare("" +a + b);
+				
+				assertEquals(square, squareGot);
+			}
+		}
+	}
 }

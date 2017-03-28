5924979167df2703b5d8787f061d79afe245a9cf
Fred Zhang <fredzqm@gmail.com>
2017-03-28 14:01:31 -0400

    add some comment for IChessViewerControl

--- src/main/java/view/IChessViewerControl.java
+++ src/main/java/view/IChessViewerControl.java
@@ -1,10 +1,22 @@
 package view;
 
-
 public interface IChessViewerControl {
 
+	/**
+	 * Invoked when view receive input from command line
+	 * 
+	 * @param input
+	 * @param isWhiteView
+	 */
 	void handleCommand(String input, boolean isWhiteView);
 
-	void click(SquareLabel squareLabel, boolean isWhite);
+	/**
+	 * Invoked when chess board is clicked
+	 * 
+	 * @param squareLabel
+	 *            the label that got clicked
+	 * @param isWhiteView
+	 */
+	void click(SquareLabel squareLabel, boolean isWhiteView);
 
 }

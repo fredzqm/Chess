367f7c6eb567511ae49c2a2293b48cf2acf9d497
Fred Zhang <fredzqm@gmail.com>
2017-04-19 18:47:28 -0400

    remove actuall MovePatternMatcher implementation

--- jsweet/src/main/java/model/MovePatternMatcher.java
+++ jsweet/src/main/java/model/MovePatternMatcher.java
@@ -1,23 +1,14 @@
 package model;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class MovePatternMatcher implements IMovePatternMatcher {
-	private static Pattern pattern =
-			Pattern.compile("([PRNBQK])?([a-h])?([1-8])?([-x])?([a-h][1-8])(=[RNBQ])?(.*)");
-	
-	private Matcher match;
-	
 	public MovePatternMatcher(String moveCommand) {
-		this.match = pattern.matcher(moveCommand);
 	}
 	
 	public String getGroup(int i) {
-		return match.group(i);
+		return null;
 	}
 	
 	public boolean matches() {
-		return match.matches();
+		return true;
 	}
 }

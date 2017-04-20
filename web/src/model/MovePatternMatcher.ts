47e9fa16162668ab6f201c60f29695a1e3ae2a1d
Johnathon L Hein <heinjl@rose-hulman.edu>
2017-04-20 18:50:26 -0400

    Fixed MovePatternMatcher.ts to implement regular expressions for Typescript.

--- web/src/model/MovePatternMatcher.ts
+++ web/src/model/MovePatternMatcher.ts
@@ -1,15 +1,19 @@
 /* Generated from Java with JSweet 2.0.0-SNAPSHOT - http://www.jsweet.org */
 import { IMovePatternMatcher } from './IMovePatternMatcher'; 
 export class MovePatternMatcher implements IMovePatternMatcher {
+    /*private*/ matcher : string[];
+    
     public constructor(moveCommand : string) {
+        var myRegexp = /([PRNBQK])?([a-h])?([1-8])?([-x])?([a-h][1-8])(=[RNBQ])?(.*)/g;
+        this.matcher = myRegexp.exec(moveCommand);
     }
 
     public getGroup(i : number) : string {
-        return null;
+        return this.matcher[i];
     }
 
     public matches() : boolean {
-        return true;
+        return this.matcher != null;
     }
 }
 MovePatternMatcher["__class"] = "model.MovePatternMatcher";

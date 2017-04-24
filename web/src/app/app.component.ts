7f05e8cfd7898e501d384f4817407d20e28deb9c
Coco Liu <liuh3@rose-hulman.edu>
2017-04-23 20:01:51 -0400

    example of firebase working

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
 
+export class AppComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/items');
+  }
   ngOnInit() {
 
   }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { SquareComponent } from './chess/board/square/square.component';
 import { BoardComponent } from './chess/board/board.component';
 import { ConsoleComponent } from './chess/console/console.component';
 import { ChessComponent } from './chess/chess.component';
+import { firebaseConfig } from '../environments/firebase.config';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { ChessComponent } from './chess/chess.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
